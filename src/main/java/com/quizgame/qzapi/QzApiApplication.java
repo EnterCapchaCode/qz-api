fcb367cdeea10a36358f20c70654d453d5682d3c
Denis Turbal <dturball@gmail.com>
2022-10-08 18:57:01 +0300

    Initial commit

---
+++ src/main/java/com/quizgame/qzapi/QzApiApplication.java
@@ -0,0 +1,13 @@
+package com.quizgame.qzapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QzApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QzApiApplication.class, args);
+	}
+
+}
